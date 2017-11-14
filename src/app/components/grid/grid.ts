0b8808e7096fc654621dce80ac3d2960d3289d31
Nils Bifano <nilsbifano@gmail.com>
2017-11-14 14:24:49 -0700

    Grid now initializes bombs and bomb-adjacency numbers on tiles.

--- src/app/components/grid/grid.ts
+++ src/app/components/grid/grid.ts
@@ -21,12 +21,52 @@ export default class GridComponent {
       }
     }
     this.initBombs(10);
+    this.initBombAdjacencies();
   }
 
   public initBombs = function(num: number){
+    while (num > 0) {
+      let coord = this.getRandomTileCoord();
+      if(this.tiles[coord.row][coord.col].isBomb){
+        // keep looking
+      } else {
+        this.tiles[coord.row][coord.col].isBomb = true;
+        this.tiles[coord.row][coord.col].text = "💣";
+        console.log("created bomb at " + coord.col + "," + coord.row);
+        num--;
+      }
+    }
+  }
 
+  public initBombAdjacencies = function(){
+    for(var y: number = 0; y < this.height; y++) {
+      for(var x: number = 0; x < this.width; x++) {
+        let tile = this.tiles[y][x];
+        if(!tile.isBomb){
+          if(x-1 >= 0 && y+1 < this.height && this.tiles[y+1][x-1].isBomb){ tile.numAdjacentBombs++; }
+          if(x-1 >= 0 && this.tiles[y][x-1].isBomb){ tile.numAdjacentBombs++; }
+          if(x-1 >= 0 && y-1 >= 0 && this.tiles[y-1][x-1].isBomb){ tile.numAdjacentBombs++; }
+          if(y-1 >= 0 && this.tiles[y-1][x].isBomb){ tile.numAdjacentBombs++; }
+          if(x+1 < this.width && y-1 >= 0 && this.tiles[y-1][x+1].isBomb){ tile.numAdjacentBombs++; }
+          if(x+1 < this.width && this.tiles[y][x+1].isBomb){ tile.numAdjacentBombs++; }
+          if(x+1 < this.width && y+1 < this.height && this.tiles[y+1][x+1].isBomb){ tile.numAdjacentBombs++; }
+          if(y+1 < this.height && this.tiles[y+1][x].isBomb){ tile.numAdjacentBombs++; }
+          if(tile.numAdjacentBombs > 0){
+            tile.text = "" + tile.numAdjacentBombs;
+          }
+        }
+      }
+    }
+  }
+
+  public getRandomTileCoord() {
+    let randomRow = Math.floor(Math.random() * (this.height));
+    let randomCol = Math.floor(Math.random() * (this.width));
+    return {row: randomRow, col: randomCol};
   }
 }
+
+
 
 class Tile {
   x: number;
